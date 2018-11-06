9bf13b6a57305fcdefea49fdda8b69f3b55d24e7
Suhrab Kurbanov <sir.suhrab@gmail.com>
2018-11-06 00:46:57 -0600

    transfered common html to base.html

--- django_portfolio/portfolio/views.py
+++ django_portfolio/portfolio/views.py
@@ -6,5 +6,6 @@ from django.shortcuts import render
 def portfolio(request):
     return render(request, 'portfolio/portfolio.html')
 
+
 def home(request):
     return render(request, 'portfolio/home.html')
